d1ffe757f3d3764dded63f45c99fe338146865f7
Océane Bertrand <oceanebertrand339@gmail.com>
2025-01-30 15:56:51 +0100

    ✨ Ajout d'un controller pour supprimer les sauces 🍲 et ses tests unitaires 🧪

--- app/controllers/sauces_controller.rb
+++ app/controllers/sauces_controller.rb
@@ -1,5 +1,6 @@
 class SaucesController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: [:create]
+    skip_before_action :verify_authenticity_token, only: [:create, :destroy]
+    before_action :set_sauce, only: %i[destroy]
 
     def create
       @sauce = Sauce.new(sauce_params)
@@ -9,8 +10,24 @@ class SaucesController < ApplicationController
         render json: @sauce.errors, status: :unprocessable_entity
       end
     end
+
+    def destroy
+        if @sauce.destroy
+            head :no_content
+        else
+            render json: { error: "Failed to delete sauce" }, status: :unprocessable_entity
+        end
+    end
   
     private
+
+    def set_sauce
+        @sauce = Sauce.find_by(id: params[:id])
+        unless @sauce
+          render json: { error: 'Sauce not found' }, status: :unprocessable_entity
+        end
+    end
+      
   
     def sauce_params
       params.require(:sauce).permit(:nom, :description, :ingredients, :caracteristique, :quantite, :prix)
--- spec/controllers/sauces_controllers_spec.rb
+++ spec/controllers/sauces_controllers_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe SaucesController, type: :controller do
-  let(:valid_params) do { sauce: {nom: 'Sauce tomate', quantite: 500, prix: 3.99, description: 'Delicious tomato sauce', ingredients: 'Tomatoes, salt', caracteristique: 'Spicy'} } end
-  let(:invalid_params) do { sauce: {nom: '', description: '', ingredients: '', caracteristique: '', quantite: nil, prix: nil}} end
+  let(:valid_params) { { sauce: attributes_for(:sauce) } }
+  let(:invalid_params) { {sauce: attributes_for(:sauce_with_empty_fields)} }
+  let!(:sauce) { create(:sauce) }
 
   describe 'POST #create' do
     context 'with valid parameters' do
@@ -27,4 +28,20 @@ RSpec.describe SaucesController, type: :controller do
       end
     end
   end
+
+  describe "DELETE #destroy" do
+    context "when the sauce exists" do
+      it "destroys the sauce and returns no content status" do
+        expect { delete :destroy, params: { id: sauce.id } }.to change(Sauce, :count).by(-1)
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+
+    context "when the sauce doesn't exist" do
+      it "returns an error message with unprocessable entity status" do
+        expect { delete :destroy, params: { id: 999999 } }.not_to change(Sauce, :count)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+end
 end
